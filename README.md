9242d9aa03756dbef1e9db374b6cb1f0b8372780
ChickenChamp71 <chickensandsharks@gmail.com>
2023-03-31 02:07:21 -0700

    forgot to add everything to repo, here is the completed project

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# password-generator-challenge-3
+# Password Generator
+
+## Description
+
+A random password generator that takes is password length, as well as if you want uppercase, lowercase, numbers, or special characters.
+
+## Installation
+
+N/A
+
+## Usage
+
+Follow the prompts on the screen to create your password!
+
+![Image with what the page looks like:](./assets/images/password-gen-1.png)
+
+![Image of sample password:](./assets/images/password-gen-2.png)
+
+## Credits
+
+N/A
+
+## License
+
+See license in repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,128 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength
+var isLowercase
+var isUppercase
+var isNumbers
+var isSpecialCharacters
+var passwordBase = [];
+var password = "";
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split('');
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+var numbers = "1234567890".split('');
+var specialCharacters =  "~`!@#$%^&*_-+=<>.?/".split('');
+var resetPass = "";
+var passwordText = document.querySelector("#password");
+
+function promptLength() {
+  passwordLength = prompt("How long would you like your password? Choose between 8 and 128 characters.");
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+     return;
+  } else {
+    alert("Not a valid input! Please try again.");
+    promptLength();
+  }
+
+};
+
+function isLowercaseTrue() {
+  var isTrue = confirm("Would you like lowercase letters?");
+
+  if (isTrue) {
+    isLowercase = true;
+  } else {
+    isLowercase = false;
+  }
+};
+
+function isUppercaseTrue() {
+  var isTrue = confirm("Would you like uppercase letters?");
+
+  if (isTrue) {
+    isUppercase = true;
+  } else {
+    isUppercase = false;
+  }
+};
+
+function isNumbersTrue() {
+  var isTrue = confirm("Would you like numbers?");
+
+  if (isTrue) {
+    isNumbers = true;
+  } else {
+    isNumbers = false;
+  }
+};
+
+function isSpecialCharactersTrue() {
+  var isTrue = confirm("Would you like special characters?")
+
+  if (isTrue) {
+    isSpecialCharacters = true;
+  } else {
+    isSpecialCharacters = false;
+  }
+};
+
+
+
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+  passwordText.value = resetPass;
+
+  promptLength();
+  isLowercaseTrue();
+  isUppercaseTrue();
+  isNumbersTrue();
+  isSpecialCharactersTrue();
+
+  if (!isLowercase && !isUppercase && !isNumbers && !isSpecialCharacters) {
+
+    alert("You must choose at least one character type! Please try again.");
+    writePassword();
+
+  } else {
+
+    if (isLowercase) {
+      passwordBase = passwordBase.concat(lowercase);
+    }
+
+    if (isUppercase) {
+      passwordBase = passwordBase.concat(uppercase);
+    }
+
+    if (isNumbers) {
+      passwordBase = passwordBase.concat(numbers);
+    }
+
+    if (isSpecialCharacters) {
+      passwordBase = passwordBase.concat(specialCharacters);
+    }
+
+    function randomPassword() {
+
+      for (let i = 0; i < passwordLength; i++) {
+        password += passwordBase[Math.floor(Math.random() * passwordBase.length)];
+      }
+
+    }
+
+    randomPassword();
+
+    console.log(password);
+
+  }
+
+ 
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
